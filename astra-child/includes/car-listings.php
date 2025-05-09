1736a3b09c12faec7b23d5023a430ee76fea33c8
llkotidis <leonidaskotidis4@gmail.com>
2025-05-09 18:36:57 +0300

    car submission date implemented

--- astra-child/includes/car-listings.php
+++ astra-child/includes/car-listings.php
@@ -166,6 +166,13 @@ function display_car_listings($atts) {
                                     </div>
                                 </div>
                                 <div class="car-price">€<?php echo number_format($price); ?></div>
+                                <?php 
+                                $publication_date = get_post_meta(get_the_ID(), 'publication_date', true);
+                                if ($publication_date) {
+                                    $formatted_date = date_i18n('F j, Y', strtotime($publication_date));
+                                    echo '<div class="car-publication-date">Listed on ' . esc_html($formatted_date) . '</div>';
+                                }
+                                ?>
                                 <div class="car-location"><?php echo esc_html($location); ?></div>
                             </div>
                         </a>
