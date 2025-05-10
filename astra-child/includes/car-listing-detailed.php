478df1881509b5d33cbfddf5d5948d5091af3cb6
llkotidis <leonidaskotidis4@gmail.com>
2025-05-10 12:23:43 +0300

    added the date in the car listing detailed

--- astra-child/includes/car-listing-detailed.php
+++ astra-child/includes/car-listing-detailed.php
@@ -153,6 +153,15 @@
                         </div>
 
                         <div class="car-price">€<?php echo number_format($price); ?></div>
+                        <?php 
+                        $publication_date = get_post_meta($car_id, 'publication_date', true);
+                        if (!$publication_date) {
+                            $publication_date = get_the_date('Y-m-d H:i:s');
+                            update_post_meta($car_id, 'publication_date', $publication_date);
+                        }
+                        $formatted_date = date_i18n('F j, Y', strtotime($publication_date));
+                        echo '<div class="car-publication-date">Listed on ' . esc_html($formatted_date) . '</div>';
+                        ?>
                         <div class="car-location"><?php echo esc_html($location); ?></div>
                     </div>
                 </div>
@@ -405,6 +414,12 @@
             .car-location {
                 color: #666;
                 font-size: 1.1em;
+            }
+
+            .car-publication-date {
+                color: #666;
+                font-size: 1.1em;
+                margin-bottom: 10px;
             }
 
             .car-listing-details {
